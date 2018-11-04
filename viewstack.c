ee18181c9f9b4952b7dc2a74e699b12b8678529b
Tinou <tinoCucaAs@gmTail.com (take the cat out)>
2018-11-05 00:21:16 +0100

    don't require completely distinct view for toggle
    stack ordering is enough

--- viewstack.c
+++ viewstack.c
@@ -140,12 +140,6 @@ findtoggletagset (Monitor *m) {
 	unsigned int tag = 0;
 	ViewStack* v = m->vs;
 
-	/* rewing stack for non-empty tagset with no tags in common */
-	while (toggletags == curseltags && v) {
-		if ((v->tagset & curseltags) == 0 && hasclientson(m, v->tagset))
-			toggletags = v->tagset;
-		v = v->next;
-	}
 	/* rewind stack for different non-empty tagset that share some tags */
 	v = m->vs;
 	while (toggletags == curseltags && v) {
