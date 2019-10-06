2fe8407255e48c1b530cb6febe07d68ccf2c7cab
tinoucas <tinoucas@gmail.com>
2019-10-06 16:41:39 +0200

    changed rofi shortcut and command

--- config.h
+++ config.h
@@ -141,7 +141,7 @@ static const char *updatedpicmd[] = { "/bin/sh", "-c", "/home/tinou/hacks/script
 static const char *killclockscmd[] = { "killall", "oclock", NULL };
 static const char *showplankcmd[] = { "/bin/zsh", "/home/tinou/hacks/scripts/showPlank.sh", "show", NULL };
 static const char *hideplankcmd[] = { "/bin/zsh", "/home/tinou/hacks/scripts/showPlank.sh", "hide", NULL };
-static const char *rofiwindowcmd[] = { "rofi", "-show", "window", NULL };
+static const char *rofiwindowcmd[] = { "rofi", "-modi", "combi", "-show", "combi", "-combi-modi", "window,drun" , NULL };
 static const Rule clockrule =
 	/* class           , instance            , title       , tags mask , float , term  , noswl , trnsp , nofcs , nobdr , rh    , mon , remap  , preflt    , istrans , procname */
     {  NULL            , "oclock"            , NULL        , alltags   , True  , False , True  , SPCTR , True  , True  , True  ,-1   , NULL   , NULL      , False   , NULL };
@@ -161,7 +161,7 @@ static Key keys[] = {
     { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
     { MODKEY,                       XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
-    { Mod1Mask,                     XK_Tab,    spawn,          {.v = rofiwindowcmd } },
+    { MODKEY,                       XK_space,  spawn,          {.v = rofiwindowcmd } },
     { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[TILE]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[FLOAT]} },
@@ -169,7 +169,6 @@ static Key keys[] = {
     { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[SPIRAL]} },
     { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[DWINDLE]} },
     { MODKEY,                       XK_BackSpace, rotatemonitor, {.i = 0} },
-    { MODKEY,                       XK_space,  rotatemonitor,  {.i = 1} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY|ControlMask|ShiftMask, XK_space,  allnonfloat,       {0} },
     //{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
