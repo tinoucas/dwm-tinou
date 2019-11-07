faa56edde981cbc8fa0fc96030fb829b8a258ad4
Tinou <tinoCucaAs@gmTail.com (take the cat out)>
2019-11-07 22:16:20 +0100

    fixed build

--- dwm.c
+++ dwm.c
@@ -2542,7 +2542,7 @@ setfullscreen(Client *c, Bool fullscreen) {
 		c->bw = 0;
 		c->isfloating = True;
 		c->tags = vtag;
-		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mho);
+		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
 		XRaiseWindow(dpy, c->win);
 		monview(c->mon, vtag);
 	}
