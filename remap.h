2555763c1996297974c9c2f08adead46fc9e0f52
Tinou <tinoCucaAs@gmTail.com (take the cat out)>
2019-12-25 21:53:40 +0100

    changed some mouse mappings

--- remap.h
+++ remap.h
@@ -9,8 +9,10 @@ static const Remap mpv[] = {
 };
 
 static const Remap sView[] = {
-	{ Button11, XK_k, XK_Page_Up, 0},
-	{ Button10, XK_j, XK_Page_Down, 0},
+	{ Button8, 0, XK_Page_Up, 0},
+	{ Button9, 0, XK_Page_Down, 0},
+	{ Button11, 0, XK_Page_Up, 0},
+	{ Button10, 0, XK_Page_Down, 0},
 	{ Button2, 0,  XK_Escape, 0 },
 	{ Button4, 0,  XK_Left,   0 },
 	{ Button5, 0,  XK_Right,  0 },
@@ -20,9 +22,17 @@ static const Remap sView[] = {
 static const Remap chrome[] = {
 	{ Button11, 0,  XK_k, 0},
 	{ Button10, 0,  XK_j, 0},
-	{ Button9, 0, XK_k, 0},
-	{ Button8, 0, XK_j, 0},
+	{ Button8, 0, XK_k, 0},
+	{ Button9, 0, XK_j, 0},
 	{ Button12, 0, XK_r, 0},
+	{ 0 },
+};
+
+static const Remap plex[] = {
+	{ Button8, 0, XK_Left, ShiftMask},
+	{ Button9, 0, XK_Right, ShiftMask},
+	{ Button11, 0, XK_Left, ShiftMask},
+	{ Button10, 0, XK_Right, ShiftMask},
 	{ 0 },
 };
 
@@ -36,4 +46,5 @@ remaps[] = {
 	REMAP(mpv),
 	REMAP(sView),
 	REMAP(chrome),
+	REMAP(plex),
 };
