47d35c8da40a1c775f8048efa0eff8ce09d4ca10
Tinou <tinoCucaAs@gmTail.com (take the cat out)>
2020-03-17 13:20:04 +0100

    inverted j/k mouse mappings

--- remap.h
+++ remap.h
@@ -4,14 +4,14 @@ static const Remap mpv[] = {
 	/*{ Button3, 0,  XK_f,     0 },*/
 	{ Button11, 0,  XK_k, 0},
 	{ Button10, 0,  XK_j, 0},
-	{ Button8, 0, XK_k, 0},
-	{ Button9, 0, XK_j, 0},
+	{ Button9, 0, XK_k, 0},
+	{ Button8, 0, XK_j, 0},
 	{ 0 },
 };
 
 static const Remap sView[] = {
-	{ Button8, 0, XK_Page_Up, 0},
-	{ Button9, 0, XK_Page_Down, 0},
+	{ Button9, 0, XK_Page_Up, 0},
+	{ Button8, 0, XK_Page_Down, 0},
 	{ Button11, 0, XK_Page_Up, 0},
 	{ Button10, 0, XK_Page_Down, 0},
 	{ Button2, 0,  XK_Escape, 0 },
@@ -23,15 +23,15 @@ static const Remap sView[] = {
 static const Remap chrome[] = {
 	{ Button11, 0,  XK_k, 0},
 	{ Button10, 0,  XK_j, 0},
-	{ Button8, 0, XK_k, 0},
-	{ Button9, 0, XK_j, 0},
+	{ Button9, 0, XK_k, 0},
+	{ Button8, 0, XK_j, 0},
 	{ Button12, 0, XK_r, 0},
 	{ 0 },
 };
 
 static const Remap plex[] = {
-	{ Button8, 0, XK_Left, ShiftMask},
-	{ Button9, 0, XK_Right, ShiftMask},
+	{ Button9, 0, XK_Left, ShiftMask},
+	{ Button8, 0, XK_Right, ShiftMask},
 	{ Button11, 0, XK_Left, ShiftMask},
 	{ Button10, 0, XK_Right, ShiftMask},
 	{ 0 },
